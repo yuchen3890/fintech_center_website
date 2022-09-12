049bd5a51bbfa6338c723e80997e70c39339be17
yuchen3890 <alice95058@gmail.com>
2022-09-12 16:54:45 +0800

    unify the name of avatars

--- src/Components/info_all_members.js
+++ src/Components/info_all_members.js
@@ -9,7 +9,7 @@ const HEAD_MEMBERS = [
         bio:
             '',
         avatar:
-            '/Chu-Song-Chen.jpg',
+            '/chu-song-chen.jpg',
     },
     {
         name_ch: '陳業寧',
@@ -33,7 +33,7 @@ const HEAD_MEMBERS = [
         bio:
             "邵慶平教授是臺灣大學法學士、法學碩士，美國柏克萊加州大學法學碩士（LLM）、法學博士（SJD），曾任國際通商法律事務所律師、中正大學法律學系教授。自2011年起任教於臺灣大學法律學院，曾兼任企業暨金融法制研究中心主任、NTU Law Review主編，目前兼任副院長一職。<p style='text-align: justify;'>邵教授曾獲得中正大學青年學者獎、科技部優秀年輕學者研究計畫、臺灣大學教學傑出獎。主要研究領域包括公司法、證券交易法、金融法。我國公司法「閉鎖性股份有限公司專節」的立法，邵教授是主要的倡議者與專節條文的起草者。",
         avatar:
-            '/Ching-Ping-Shao.jpg',
+            '/ching-ping-shao.jpg',
     },
 ];
 
@@ -60,7 +60,7 @@ const FINTECH_MEMBERS = [
         bio:
             '張智星教授於 1992 年取得加州大學柏克萊分校的電機電腦博士，1993 年的 ANFIS 論文在 Google Scholar 被引用次數超過一萬次。張教授於 1993 年加入美國 MathWorks 公司，開發與 MATLAB 共用的模糊邏輯工具箱。1995 年回台任教後，專注於機器學習的各項應用，包含語音、音樂、文件、影像辨識等領域。他曾經擔任 2014 及 2017 年 ISMIR 的大會主席及共同主席，他的團隊也在歷年 MIREX 國際音樂檢索評比中的數個項目拿下第一名的佳績。目前張教授執行多項與金融科技相關之計畫，包含精準行銷、搜尋與推薦，並擔任台大醫院資訊室主任，以及工研院資通所的顧問。',
         avatar:
-            '/Jyh-Shing-Jang.jpg',
+            '/jyh-shing-jang.jpg',
     },
     {
         name_ch: '陳君明',
@@ -84,7 +84,7 @@ const FINTECH_MEMBERS = [
         bio:
             '林守德教授擁有台灣大學電機工程學士、密西根大學電機碩士、南加州大學計算語言學碩士學位和計算機科學博士學位，目前為國立臺灣大學資訊工程學系的教授，他創立台大的機器發明與社群網路探勘實驗室。在加入台大之前，他是羅沙拉摩斯國家實驗室的博士後研究員。林教授的研究包括機器學習和資料探勘、社群網絡分析和自然語言處理等領域。他的國際學術成就包括 2003 年 IEEE 網路智能會議最佳論文獎、2007 年 Google 研究獎、三度獲得微軟研究獎、IBM 研究獎助，2010 年及 2014 TAAI 優秀論文獎、2011 年 ASONAM 最佳論文獎，以及連續 5 年獲得美國空軍 AFOSR / AOARD 研究獎。他亦是 ACM KDD Cup 的 All Time Leader，領導或共同領導台大團隊贏得 5 次冠軍，今年也帶領研究團隊贏得 2016 年 WSDM Cup。他也曾獲得科技部吳大猷先生紀念獎，傑出人才基金會年輕學者研究獎，曾擔任 SIGKDD 的 senior program chair 及 ACL 的 area chair，目前是國際社群網絡探勘期刊、資訊科學與工程期刊和中文計算語言學期刊的副主編，也是科學人的「網路不打烊」單元作家之一。',
         avatar:
-            '/Shou-De-Lin.jpg',
+            '/shou-de-lin.jpg',
     },
     {
         name_ch: '蕭旭君',
@@ -94,7 +94,7 @@ const FINTECH_MEMBERS = [
         expertise: "資訊安全、自動化軟體漏洞分析、隱私防護",
         website: "https://www.csie.ntu.edu.tw/~hchsiao",
         bio:
-            "Dr. Hsu-Chun Hsiao is an Associate Professor in the Department of Computer Science and Information Engineering, and the Graduate Institute of Networking and Multimedia at National Taiwan University. She also holds an adjunct researcher position in the Center of Information Technology and Innovation at Academia Sinica. Dr. Hsiao completed her B.S. (2006) and M.S. (2008) at National Taiwan University and Ph.D. at Carnegie Mellon University (2014). Dr. Hsiao's research interests lie in the field of network and systems security, and her recent work focuses on DDoS defense, IoT security, and automated vulnerability discovery. She is a recipient of the MOST Young Scholar Fellowship (2018-2023), K. T. Li Young Researcher Award by IICM and ACM Taipei/Taiwan Chapter (2018), and the NTU Excellent Teaching Award (2016, 2017). She runs the Network Security Lab (<a href=https://nslab.csie.ntu.edu.tw>https://nslab.csie.ntu.edu.tw</a>) at NTU and the lab's security competition team, Balsn, has won several  competitions and ranked highly internationally (<a href=https://balsn.tw>https://balsn.tw</a>).",
+            "Dr. Hsu-Chun Hsiao is an Associate Professor in the Department of Computer Science and Information Engineering, and the Graduate Institute of Networking and Multimedia at National Taiwan University. She also holds an adjunct researcher position in the Center of Information Technology and Innovation at Academia Sinica. Dr. Hsiao completed her B.S. (2006) and M.S. (2008) at National Taiwan University and Ph.D. at Carnegie Mellon University (2014). Dr. Hsiao's research interests lie in the field of network and systems security, and her recent work focuses on DDoS defense, IoT security, and automated vulnerability discovery. She is a recipient of the MOST Young Scholar Fellowship (2018-2023), K. T. Li Young Researcher Award by IICM and ACM Taipei/Taiwan Chapter (2018), and the NTU Excellent Teaching Award (2016, 2017). She runs the Network Security Lab (https://nslab.csie.ntu.edu.tw) at NTU and the lab's security competition team, Balsn, has won several  competitions and ranked highly internationally (https://balsn.tw).",
         avatar:
             '/hsu-chun-hsiao.jpg',
     },
@@ -106,7 +106,7 @@ const FINTECH_MEMBERS = [
         expertise: "語言理解、對話系統、機器智慧、自然語言處理",
         website: "https://www.csie.ntu.edu.tw/~yvchen",
         bio:
-            "陳縕儂 (Yun-Nung Vivian Chen) 現為國立台灣大學資訊工程學系助理教授，2015 年於美國卡內基美隆大學之電腦科學院獲頒博士學位，而後加入美國微軟研究院之深度學習科技中心任研究職務。研究專長包含對話系統、語言理解、以及深度學習，曾獲 Google Faculty Award、傑出人才年輕學者創新獎、科技部年輕學者獎及多次最佳論文獎。欲瞭解陳博士的研究及經歷，請至她的個人網頁 <a href=http://vivianchen.idv.tw>http://vivianchen.idv.tw</a>。",
+            "陳縕儂 (Yun-Nung Vivian Chen) 現為國立台灣大學資訊工程學系助理教授，2015 年於美國卡內基美隆大學之電腦科學院獲頒博士學位，而後加入美國微軟研究院之深度學習科技中心任研究職務。研究專長包含對話系統、語言理解、以及深度學習，曾獲 Google Faculty Award、傑出人才年輕學者創新獎、科技部年輕學者獎及多次最佳論文獎。欲瞭解陳博士的研究及經歷，請至她的個人網頁 http://vivianchen.idv.tw。",
         avatar:
             '/yun-nung-chen.png',
     },
@@ -123,7 +123,7 @@ const FINBUSS_MEMBERS = [
         bio:
             '張森林為台灣大學電機學士、台灣大學商學研究所碩士及英國蘭開斯特大學財務博士，現任職國立台灣大學財務金融學系特聘教授及證劵期貨暨財務工程研究中心主任，並擔任Journal of Futures Markets編輯委員、「臺大管理論叢」及「財務金融學刊」副總編輯、「管理學報」、「證券市場發展季刊」及「期貨與選擇權學刊」編輯委員。曾任國立臺灣大學財務金融學系系主任、教授、國立中央大學財務金融學系副教授、助理教授。曾獲科技部「吳大猷先生紀念獎」、「傑出研究獎」及國科會「甲種研究獎」等。研究專長為財務工程、資產配置、行為財務實證研究、風險管理、隨機利率模型；研究成果發表於Journal of Financial and Quantitative Analysis、Management Science、 Journal of Banking and Finance、Journal of Empirical Finance、Journal of Futures Markets、 Journal of Derivatives、Financial Analyst Journal、Quantitative Finance、Review of Derivatives Research、財務金融學刊、證劵市場發展季刊、經濟論文、管理學報、臺大管理論叢、期貨與選擇權學刊等學術期刊。近年研究興趣為: Black-Litterman模型的運用分析、機器人理財的績效評估、選擇權價量資訊的運用分析、機器學習和技術分析的選股策略研究、buy and hold策略的賣出時點研究等。',
         avatar:
-            '/San-Lin-Chung.jpg',
+            '/san-lin-chung.jpg',
     },
     {
         name_ch: '王之彥',
@@ -135,7 +135,7 @@ const FINBUSS_MEMBERS = [
         bio:
             '王之彥為國立臺灣大學資訊工程學士、碩士及國立臺灣大學國際企業學系財務博士，現任職國立臺灣大學國際企業學系教授，曾任國立臺灣大學國際企業學系副教授、助理教授、國立臺灣科技大學財務金融研究所助理教授、國立中興大學財務金融學系助理教授。曾獲臺灣大學管理學院玉山學術獎。研究專長為財務工程、信用風險模型、隨機利率模型、資產定價、行為財務學；研究成果發表於Management Science、 Journal of Banking and Finance、Journal of Futures Markets、 Journal of Derivatives、Review of Derivatives Research、Annals of Operations Research等學術期刊。近年研究興趣為: 風險態度與選擇權合理價格、選擇權隱含資訊、價量資訊的運用分析、高頻交易等。',
         avatar:
-            '/jr-yan.png',
+            '/jr-yan-wang.png',
     },
     {
         name_ch: '莊文議',
@@ -147,7 +147,7 @@ const FINBUSS_MEMBERS = [
         bio:
             '',
         avatar:
-            '/wen-i.png',
+            '/wen-i-chuang.png',
     },
 ]
 
@@ -163,7 +163,7 @@ const FINLAW_MEMBERS = [
         bio:
             '林仁光教授為美國波士頓大學國際銀行法法學碩士(LL.M. in International Banking Law Studies)、美國杜克大學法學碩士(LL.M.) 、法學博士(S.J.D.)。2003年起任職於國立臺灣大學，曾兼任國立臺灣大學企業暨金融法制研究中心主任、商事法中心主任；中華民國投信投顧公會理事；財團法人證券投資人及期貨交易人保護中心董事、調處委員；台灣法學會商事法委員會主任委員、財經法委員會主任委員；經濟部貿易調查委員會委員；NTU Law Review主編。主要研究領域包括：公司法、證券交易法、銀行法、金融法、英美契約法、比較企業金融法制、亞洲新興市場企業金融法制。',
         avatar:
-            '/Jen-Guang-Lin.jpg',
+            '/jen-guang-lin.jpg',
     },
     {
         name_ch: '汪信君',
@@ -214,7 +214,7 @@ const INDUSTRY_MEMBERS = [
         bio:
             'Yung-Yaw Chen (陳永耀) received the B.S. degree in electrical engineering from National Taiwan University in 1981 and the Ph.D. degree in electrical engineering and computer sciences from University of California at Berkeley in 1989. He is currently a professor of the department of electrical engineering, National Taiwan University, Taipei, Taiwan, where he does research on intelligent control, fuzzy logic, computational intelligence, precision servo control, hyperthermia treatment planning, and augmented reality mini-invasive surgical system. He has published over 130 papers, including about 40 journal papers in these areas. He received the Excellent Research Awards from National Science Council in 1990 and 1991. He acted as the Program Chair in 1996 Asian Fuzzy Systems Symposium and Vice Program Chair in 2000 IFSA conference and also served as an associate editor in International Journal of Fuzzy Systems. He is a member of the IEEE Control Systems Society, Computer Society, Neural Networks Society, Systems, Man, and Cybernetics Society, and Ultrasound society.',
         avatar:
-            '/Yung-Yaw-Chen.png',
+            '/yung-yaw-chen.png',
     },
     {
         name_ch: '張智星',
@@ -226,7 +226,7 @@ const INDUSTRY_MEMBERS = [
         bio:
             '張智星教授於 1992 年取得加州大學柏克萊分校的電機電腦博士，1993 年的 ANFIS 論文在 Google Scholar 被引用次數超過一萬次。張教授於 1993 年加入美國 MathWorks 公司，開發與 MATLAB 共用的模糊邏輯工具箱。1995 年回台任教後，專注於機器學習的各項應用，包含語音、音樂、文件、影像辨識等領域。他曾經擔任 2014 及 2017 年 ISMIR 的大會主席及共同主席，他的團隊也在歷年 MIREX 國際音樂檢索評比中的數個項目拿下第一名的佳績。目前張教授執行多項與金融科技相關之計畫，包含精準行銷、搜尋與推薦，並擔任台大醫院資訊室主任，以及工研院資通所的顧問。',
         avatar:
-            '/Jyh-Shing-Jang.jpg',
+            '/jyh-shing-jang.jpg',
     },
     {
         name_ch: '王鈺強',
@@ -238,7 +238,7 @@ const INDUSTRY_MEMBERS = [
         bio:
             "Yu-Chiang Frank Wang received his B.S. degree in Electrical Engineering from National Taiwan University in 2001. He received his M.S. and Ph.D. degrees in Electrical and Computer Engineering from Carnegie Mellon University in 2004 and 2009, respectively. In 2009, Dr. Wang joined the Research Center for Information Technology Innovation (CITI), Academia Sinica as an Assistant Research Fellow, and later he was promoted as the Associate Research Fellow in 2013, and also served as the Deputy Director of CITI from 2015 to 2017.<p style='text-align: justify;'>In 2017, Dr. Wang joins the Graduate Institute of Communication Engineering and Department of Electrical Engineering at National Taiwan Univeristy as an Associate Professor. With continuing research focuses on computer vision and machine learning, Dr. Wang's recent research topics include deep learning for transfer learning, domain adaptation, face recognition, semantic segmentation, and multi-label classification. Dr. Wang serves as organizing committee members and area chairs of multiple international conferences such as CVPR, ICCV, ECCV, and ACCV. Several of his papers are nominated for the best paper awards, including IEEE ICIP, ICME, AVSS and MVA. Dr. Wang is twice selected as the Outstanding Young Researcher by the Ministry of Science and Technology of Taiwan (2013-2015 and 2017-2019).",
         avatar:
-            '/Yu-Chiang-Wang.jpg',
+            '/yu-chiang-wang.jpg',
     },
 ]
 
@@ -249,7 +249,7 @@ const ASSISTANTS = [
         title: '秘書',
         email: 'nicolefang@ntu.edu.tw',
         phone: '(02) 3366-4888 #542',
-        avatar: '/Rong-Fang.png'
+        avatar: '/rong-fang.png'
     },
 ]
 
