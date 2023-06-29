b43844a1750756a301c5b7db799a58df4e99a553
yuchen3890 <alice95058@gmail.com>
2023-06-29 13:56:00 +0800

    change content of the chatgpt event

--- src/Activities/eventIntro.js
+++ src/Activities/eventIntro.js
@@ -2,7 +2,6 @@ import {
     Box,
     chakra,
     Flex,
-    Link,
     Image
 } from '@chakra-ui/react';
 
@@ -43,22 +42,62 @@ const EventIntro = ({ activity }) => {
                     backgroundSize: 'cover',
                     backgroundImage: "url('/quotation_marks.png')",
                 }}>
-                <Box
-                    textAlign={'left'}>
+                <Box textAlign={'left'}>
                     <chakra.p
                         fontFamily={'Inter'}
                         fontWeight={'medium'}
                         fontSize={'20px'}
                         pb={4}
                     >
-                        本論壇將於 {activity.date} 在{activity.place}舉行，歡迎大家踴躍報名參加。<br/>
-                        報名連結：<Link href='https://forms.gle/wwLXyVK1pWLYCgtUA' isExternal>
-                        https://forms.gle/wwLXyVK1pWLYCgtUA </Link>
+                        {activity.paragraph[0]}
+                    </chakra.p>
+                    <chakra.p
+                        fontFamily={'Inter'}
+                        fontWeight={'medium'}
+                        fontSize={'20px'}
+                        pb={4}
+                    >
+                        {activity.paragraph[1]}
+                    </chakra.p>
+                    <Flex
+                        pb={4}>
+                        <Box>
+                            <Image src={`../activity/${activity.pics[0]}`} alt='NONE'></Image>
+                        </Box>
+                        <Box>
+                            <Image src={`../activity/${activity.pics[1]}`} alt='NONE'></Image>
+                        </Box>
+                    </Flex>
+                    <chakra.p
+                        fontFamily={'Inter'}
+                        fontWeight={'medium'}
+                        fontSize={'20px'}
+                        pb={4}
+                    >
+                        {activity.paragraph[2]}
+                    </chakra.p>
+                    <Image pb={4} src={`../activity/${activity.pics[2]}`} alt='NONE'></Image>
+                    <chakra.p
+                        fontFamily={'Inter'}
+                        fontWeight={'medium'}
+                        fontSize={'20px'}
+                        pb={4}
+                    >
+                        {activity.paragraph[3]}
+                    </chakra.p>
+                    <Image pb={4} src={`../activity/${activity.pics[3]}`} alt='NONE'></Image>
+                    <chakra.p
+                        fontFamily={'Inter'}
+                        fontWeight={'medium'}
+                        fontSize={'18px'}
+                    >
+                        {activity.paragraph[4]} <br/>
+                        {activity.paragraph[5]}
                     </chakra.p>
                 </Box>
-                <Image src={`../activity/${activity.fileName}`} alt='NONE'></Image>
+
             </Box>
-            
+
         </Flex>
     )
 }
